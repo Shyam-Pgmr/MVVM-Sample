91c387139bbb81ddeb6441caea8af1c096f2a876
Shyam <shyam.pgmr@gmail.com>
2019-04-05 16:50:48 +0530

    Base setup

---
+++ MVVM Sample/Network/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM Sample
+//
+//  Created by Shyam Kumar on 05/04/19.
+//  Copyright © 2019 Shyam Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
